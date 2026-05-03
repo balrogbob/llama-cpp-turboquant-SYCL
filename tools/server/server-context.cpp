2abdc5279707fb54f7d17a318c6e84d7d995920a
balrogbob <joshuadrichards@gmail.com>
2026-05-02 21:51:48 -0700

    Fixed backend selection for mmproj files, preventing a rare crash on image chat directly in llama.cpp webui
    Signed-off-by: balrogbob <joshuadrichards@gmail.com>

--- tools/server/server-context.cpp
+++ tools/server/server-context.cpp
@@ -910,6 +910,7 @@ private:
             mtmd_context_params mparams = mtmd_context_params_default();
 
             mparams.use_gpu          = params_base.mmproj_use_gpu;
+            mparams.backend_device   = params_base.devices.empty() ? nullptr : ggml_backend_dev_name(params_base.devices.front());
             mparams.print_timings    = false;
             mparams.n_threads        = params_base.cpuparams.n_threads;
             mparams.flash_attn_type  = params_base.flash_attn_type;
